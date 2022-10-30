03458db81af10bdc15386c15af1461d2078b3150
Валерия <lerusik434@gmail.com>
2022-10-30 22:37:02 +0400

    Добавляет исправления в модуль открытия и закрытия редакторая изображения

--- js/upload-user-img.js
+++ js/upload-user-img.js
@@ -1,36 +1,31 @@
-import './random-photo-user.js';
 import { isEscapeKey } from './utils.js';
 
-const userImgModalElement = document.querySelector('.img-upload__overlay');
-const userImgModal = document.querySelector('body');
-const userImgModalOpenElement = userImgModal.querySelector('#upload-file');
-const userImgModalCloseElement = userImgModal.querySelector('.img-upload__cancel');
+const uploadForm = document.querySelector('.img-upload__form');
+const userImgModalElement = uploadForm.querySelector('.img-upload__overlay');
+const {body} = document;
+const userImgModalOpenElement = uploadForm.querySelector('#upload-file');
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    closeUserModal();
+    uploadForm.reset();
   }
 };
 
 function openUserModal () {
   userImgModalElement.classList.remove('hidden');
-  userImgModal.classList.add('modal-open');
+  body.classList.add('modal-open');
 
   document.addEventListener('keydown', onPopupEscKeydown);
 }
 
 function closeUserModal () {
   userImgModalElement.classList.add('hidden');
-  userImgModal.classList.remove('modal-open');
+  body.classList.remove('modal-open');
 
   document.removeEventListener('keydown', onPopupEscKeydown);
 }
 
-userImgModalOpenElement.addEventListener('click', () => {
-  openUserModal();
-});
+userImgModalOpenElement.addEventListener('change', openUserModal);
 
-userImgModalCloseElement.addEventListener('click', () => {
-  closeUserModal();
-});
+uploadForm.addEventListener('reset', closeUserModal);
